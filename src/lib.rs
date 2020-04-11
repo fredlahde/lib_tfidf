b703c79ed357175902563087e9a50e7facde6f02
Fred Lahde <frederick.lahde@worldiety.de>
2020-04-12 00:25:35 +0200

    finish tfidif algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,176 +1,87 @@
-use std::collections::HashMap;
-use std::io::{Read, Result};
-trait Document<ID>: Send + Sync
+use std::collections::{HashMap, HashSet};
+use std::io::Result;
+
+pub trait Token {
+    fn get_term(&self) -> String;
+    fn get_offset_begin(&self) -> usize;
+    fn get_offset_end(&self) -> usize {
+        self.get_term().len() + self.get_offset_begin()
+    }
+    fn get_pos(&self) -> Option<String>;
+}
+
+pub trait Document<ID, TOK>: Send + Sync
 where
     ID: Into<String>,
+    TOK: Token,
 {
     fn get_id(&self) -> Box<ID>;
-    fn get_content(&self) -> Box<dyn Read>;
-
-    // TODO proper tokenizer
-    // maybe as argument with trait
-    fn tokenize(&self) -> Result<Vec<String>> {
-        let mut content = String::new();
-        self.get_content().read_to_string(&mut content)?;
-        Ok(content.split(' ').map(|s| s.to_owned()).collect())
-    }
+    fn get_content(&self) -> Vec<Box<TOK>>;
 }
 
-trait TfidfDataSource<'a, T>: Send + Sync
-where
-    T: Into<String>,
-{
-    fn get_all_documents(&'a self) -> &'a [Box<dyn Document<T>>];
-    fn get_document(&'a self, idx: T) -> Option<&'a dyn Document<T>>;
+pub struct Tfidf<'a, ID: Into<String>, TOK: Token> {
+    docs: &'a [Box<dyn Document<ID, TOK>>],
+    cache: HashMap<String, usize>,
 }
 
-#[derive(Hash, Eq, Debug)]
-struct Token {
-    term: String,
-    position: usize,
-}
-
-impl PartialEq for Token {
-    fn eq(&self, other: &Self) -> bool {
-        self.term == other.term
-    }
-}
-
-struct Tfidf<'a, ID: Into<String>> {
-    source: &'a dyn TfidfDataSource<'a, ID>,
-    cache: HashMap<Token, usize>,
-}
-
-impl<'a, ID: Into<String>> Tfidf<'a, ID> {
-    fn new(src: &'a dyn TfidfDataSource<'a, ID>) -> Self {
+impl<'a, ID: Into<String>, TOK: Token> Tfidf<'a, ID, TOK> {
+    pub fn new(src: &'a [Box<dyn Document<ID, TOK>>]) -> Self {
         Tfidf {
-            source: src,
+            docs: src,
             cache: HashMap::new(),
         }
     }
 
-    fn fit_transform(&mut self) -> Result<()> {
-        for doc in self.source.get_all_documents() {
-            for (position, term) in doc.tokenize()?.iter().enumerate() {
-                let term = term.clone();
-                let token = Token { term, position };
-                let entry = self.cache.entry(token).or_insert(0);
-                *entry += 1;
+    pub fn fit_transform(&mut self) -> Result<()> {
+        for doc in self.docs {
+            let mut seen = HashSet::new();
+            for token in doc.get_content() {
+                let term = token.get_term();
+                if !seen.contains(&term) {
+                    seen.insert(term.clone());
+                    let entry = self.cache.entry(term.clone()).or_insert(1);
+                    *entry += 1;
+                }
             }
         }
         Ok(())
     }
-    fn tfidf(&self, doc: &dyn Document<ID>, term: &Token) -> Result<f64> {
-        let tokenized = doc.tokenize()?;
-        let tf = self.tf(&tokenized, term);
+
+    pub fn tfidf_doc(&self, doc: &dyn Document<ID, TOK>, term: &dyn Token) -> Result<f64> {
+        self.tfidf(doc.get_content().as_slice(), term)
+    }
+
+    pub fn tfidf(&self, doc: &[Box<TOK>], term: &dyn Token) -> Result<f64> {
+        let tf = self.tf(doc, term);
         let idf = self.idf(term);
+        if tf * idf < 0f64 {
+            panic!("oooo {} {}", tf, idf);
+        }
 
         Ok(tf * idf)
     }
 
-    fn tf(&self, doc: &[String], term: &Token) -> f64 {
+    pub fn rank_tokens(&self, doc: &[Box<TOK>]) -> Result<HashMap<String, f64>> {
+        let mut acc = HashMap::new();
+        for t in doc {
+            acc.insert(t.get_term(), self.tfidf(&doc, t.as_ref())?);
+        }
+        Ok(acc)
+    }
+
+    fn tf(&self, doc: &[Box<TOK>], term: &dyn Token) -> f64 {
         doc.iter()
-            .filter(|s| **s == term.term)
+            .filter(|s| s.get_term() == term.get_term())
             .fold(0f64, |acc, _| acc + 1f64)
     }
 
-    fn idf(&self, term: &Token) -> f64 {
-        let f = self.cache.get(term).unwrap_or(&0);
+    fn idf(&self, term: &dyn Token) -> f64 {
+        let f = self.cache.get(term.get_term().as_str()).unwrap_or(&0);
         let f = *f as f64 + 1f64;
-        let n = self.cache.keys().len() as f64;
+        let n = self.docs.len() as f64;
 
         let div = n / f;
         div.log10()
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    struct TestDoc {
-        path: &'static str,
-    }
-
-    impl Document<String> for TestDoc {
-        fn get_content(&self) -> Box<dyn Read> {
-            Box::new(std::fs::File::open(self.path).unwrap())
-        }
-
-        fn get_id(&self) -> Box<String> {
-            Box::new(self.path.to_owned())
-        }
-    }
-    struct TestSource<ID>
-    where
-        ID: Into<String>,
-    {
-        docs: Vec<Box<dyn Document<ID>>>,
-    }
-
-    impl<'a> TfidfDataSource<'a, String> for TestSource<String> {
-        fn get_all_documents(&'a self) -> &'a [Box<dyn Document<String>>] {
-            &self.docs.as_slice()
-        }
-
-        fn get_document(&'a self, idx: String) -> Option<&'a dyn Document<String>> {
-            let idx: String = idx.into();
-            for doc in &self.docs {
-                let doc_idx: String = doc.get_id().as_ref().to_owned();
-                if doc_idx == idx {
-                    return Some(doc.as_ref());
-                }
-            }
-            return None;
-        }
-    }
-
-    #[test]
-    fn test_docs() {
-        let d = TestDoc { path: "test" };
-        let docs;
-        let source = TestSource {
-            docs: vec![Box::new(d)],
-        };
-        docs = source.get_all_documents();
-        let mut v = String::new();
-        docs[0].get_content().read_to_string(&mut v).unwrap();
-    }
-
-    #[test]
-    fn test_tfidf_construction() {
-        let test_doc_fn = || TestDoc { path: "test" };
-        let source = TestSource {
-            docs: vec![Box::new(test_doc_fn()), Box::new(test_doc_fn())],
-        };
-        let tfidf = Tfidf::new(&source);
-    }
-    #[test]
-    fn test_tfidf_fit_transform() {
-        let test_doc_fn = || TestDoc { path: "test" };
-        let source = TestSource {
-            docs: vec![Box::new(test_doc_fn()), Box::new(test_doc_fn())],
-        };
-        let mut tfidf = Tfidf::new(&source);
-        tfidf.fit_transform().unwrap();
-        println!("{:?}", tfidf.cache);
-    }
-
-    #[test]
-    fn test_tfidf() {
-        let test_doc_fn = || TestDoc { path: "test" };
-        let source = TestSource {
-            docs: vec![Box::new(test_doc_fn()), Box::new(test_doc_fn())],
-        };
-        let mut tfidf = Tfidf::new(&source);
-        tfidf.fit_transform().unwrap();
-        let test_token = Token {
-            term: "Quibi".into(),
-            position: 0,
-        };
-        let got = tfidf
-            .tfidf(source.get_document("test".into()).unwrap(), &test_token)
-            .unwrap();
-        assert_eq!(got, 1f64);
     }
 }
 
